2b8657d40b9a263a187109a183cefc47115ff154
Johnny Villegas <linkrsfan@gmail.com>
2017-08-18 15:55:55 -0500

    Add files via upload

---
+++ AssymetricFractalTree/AssymetricFractalTree/Frame.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AssymetricFractalTree
+{
+    partial class Frame
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.drawbt = new System.Windows.Forms.Button();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(59, 35);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(360, 300);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Depth:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Branch length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Right angle variation:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Left angle variation:";
+            // 
+            // drawbt
+            // 
+            this.drawbt.Location = new System.Drawing.Point(192, 413);
+            this.drawbt.Name = "drawbt";
+            this.drawbt.Size = new System.Drawing.Size(75, 23);
+            this.drawbt.TabIndex = 5;
+            this.drawbt.Text = "Draw";
+            this.drawbt.UseVisualStyleBackColor = true;
+            this.drawbt.Click += new System.EventHandler(this.drawbt_Click);
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(375, 380);
+            this.nud1.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(44, 20);
+            this.nud1.TabIndex = 6;
+            this.nud1.Value = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            // 
+            // nud2
+            // 
+            this.nud2.Location = new System.Drawing.Point(169, 379);
+            this.nud2.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(44, 20);
+            this.nud2.TabIndex = 7;
+            this.nud2.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(375, 355);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(44, 20);
+            this.tb1.TabIndex = 8;
+            this.tb1.Text = "3";
+            this.tb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb1_KeyPress);
+            // 
+            // nud3
+            // 
+            this.nud3.Location = new System.Drawing.Point(167, 353);
+            this.nud3.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(46, 20);
+            this.nud3.TabIndex = 9;
+            this.nud3.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // Frame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud1);
+            this.Controls.Add(this.drawbt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Frame";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button drawbt;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.NumericUpDown nud3;
+    }
+}
+
---
+++ AssymetricFractalTree/AssymetricFractalTree/Frame.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssymetricFractalTree
+{
+    public partial class Frame : Form
+    {
+        private bool click;
+
+        public Frame()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.Text = "Asymmetric Fractal tree";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            pictureBox.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void paintAsymmetricFT(Pen pen, Graphics g, float xo, float yo, double angle, double rightAngleVar, double leftAngleVar, int depth, float length)
+        {
+            if (depth > -1)
+            {
+                float xf = xo - (cos(degreeToRadian(angle)) * length * depth);
+                float yf = yo - (sin(degreeToRadian(angle)) * length * depth);
+                g.DrawLine(pen, xo, yo, xf, yf);
+                paintAsymmetricFT(pen, g, xf, yf, angle + rightAngleVar, rightAngleVar, leftAngleVar, depth - 1, length);
+                paintAsymmetricFT(pen, g, xf, yf, angle - leftAngleVar, rightAngleVar, leftAngleVar, depth - 1, length);
+
+            }
+        }
+
+        private float cos(double angle)
+        {
+            return (float)Math.Cos(angle);
+        }
+
+        private float sin(double angle)
+        {
+            return (float)Math.Sin(angle);
+        }
+
+        private double degreeToRadian(double angle)
+        {
+            return (Math.PI * angle) / 180;
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (click)
+            {
+                float xo = (pictureBox.Width / 2) - 4;
+                float yo = pictureBox.Height - Convert.ToSingle(5.4);
+                double rightAngleVar = Convert.ToDouble(nud1.Value);
+                double leftAngleVar = Convert.ToDouble(nud2.Value);
+                int depth = Convert.ToInt32(nud3.Value);
+                float length = Convert.ToSingle(tb1.Text);
+                Pen pen = new Pen(Color.Black);
+                paintAsymmetricFT(pen, e.Graphics, xo, yo, 90, rightAngleVar, leftAngleVar, depth, length);
+                click = false;
+                pen.Dispose();
+            }
+        }
+
+        private void drawbt_Click(object sender, EventArgs e)
+        {
+            click = true;
+            pictureBox.Refresh();
+        }
+
+        private void tb1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar) && !e.KeyChar.Equals(','))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
